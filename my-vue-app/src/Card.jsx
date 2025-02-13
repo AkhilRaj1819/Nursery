1c99f9564a83139c3e89d62cb1b8a0a69440e42f
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-02-13 18:03:45 +0530

    plant

--- my-vue-app/src/Card.jsx
+++ my-vue-app/src/Card.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Card = ({plant})=>{
+    const handleAddToCart = () => {
+        addToCart(plant);
+    };
     return(
         <div>
             <img src={plant.original_url} alt=""/>
             <h5>{plant.title}</h5>
+            <button onClick={handleAddToCart}>CART</button>
         </div>
     )
 }
